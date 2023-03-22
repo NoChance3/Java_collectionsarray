bd8b1a424aaa2117318842c0412013752fbbf556
Vyasheslav Derevyanko <malorka0540205205@mail.ru>
2023-03-22 18:31:19 +0300

    done x3

--- src/main/java/practice/twoDimensionalArray/TwoDimensionalArray.java
+++ src/main/java/practice/twoDimensionalArray/TwoDimensionalArray.java
@@ -2,16 +2,19 @@ package practice.twoDimensionalArray;
 
 public class TwoDimensionalArray {
 
+
     public static final char SYMBOL = 'X';
 
     public static char[][] getTwoDimensionalArray(int size) {
-
-        //TODO: Написать метод, который создаст двумерный массив char заданного размера.
-        // массив должен содержать символ SYMBOL по диагоналям, пример для size = 3
-        // [X,  , X]
-        // [ , X,  ]
-        // [X,  , X]
-
-        return new char[0][0];
+        char [][] xxx = new char[size][size];
+        for(int i = 0; i < xxx.length; i++){
+            for (int j = 0; j < xxx[i].length; j++) {
+                xxx[i][j] = (j == i || j == xxx.length - 1 - i) ? 'X' : ' ';
+                System.out.print(xxx[i][j]);
+            }
+            System.out.println();
+        }
+        return xxx;
     }
+
 }
